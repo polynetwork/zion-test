b278b4f606e41e01b0f4c2d1a7e04ed0672e1b16
zmh <siovanus@126.com>
2023-09-19 17:25:31 +0800

    add build scripts

---
+++ build.sh
@@ -0,0 +1,24 @@
+rm -rf code
+mkdir code
+cd code
+git clone https://github.com/polynetwork/Zion.git
+cd Zion
+make geth
+cp build/bin/geth ../../
+cd ..
+git clone https://github.com/siovanus/distribute-check.git
+cd distribute-check
+go build -o distribute-check
+cp distribute-check ../../
+cd ..
+git clone https://github.com/siovanus/clean_db.git
+cd clean_db
+go build -o clean_db
+cp clean_db ../../
+cd ..
+git clone https://github.com/devfans/wizard.git
+cd wizard
+go build -o wizard
+cp wizard /usr/local/bin
+cd ../../
+go build -o zion-test
---
+++ reset.sh
@@ -0,0 +1,4 @@
+pkill -9 distribute-check
+pkill -9 geth 
+rm -rf temp
+rm -rf nohup
